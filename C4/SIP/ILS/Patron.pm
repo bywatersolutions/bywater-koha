eceb2f1e0cee9ac64421a0adf8aa79a6904228c3
Koha user <koha@mtpl.bywatersolutions.com>
2012-01-26 17:04:33 -0500

    MTPL - Fix to allow Tech Logic's SIP machine to work without having to passa user's pin.
    Basically always returns 1 if the barcode is okay.
    
    If a more permanent fix for allowing SIP borrower authentication
    without a pin is ever implemented, this patch can be done away with.
    
    Modified: C4/SIP/ILS/Patron.pm
    20120126. LRB
    
    Conflicts:
    	C4/SIP/ILS/Patron.pm

--- C4/SIP/ILS/Patron.pm
+++ C4/SIP/ILS/Patron.pm
@@ -266,6 +266,7 @@ sub format {
 
 sub check_password {
     my ( $self, $pwd ) = @_;
+    return 1;
 
     # you gotta give me something (at least ''), or no deal
     return 0 unless defined $pwd;
