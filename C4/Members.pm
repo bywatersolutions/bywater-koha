e878357855b66863c7d25f08987d39495f59ec41
Matthias Meusburger <matthias.meusburger@biblibre.com>
2012-02-15 14:57:02 +0100

    BWS-PKG - SHIB - Adds Shibboleth authentication (Matthias + JPW)
     - Use ENV{'SERVER_NAME'} instead of syspref OpacBaseURL in order to work with
     multiple vhosts.
     - Add staff client support, autoprovisioning and move settings to koha-conf.xml
    
    Conflicts:
    	C4/Members.pm
    
    Conflicts:
    	C4/Auth_with_shibboleth.pm
    
    Conflicts:
    	C4/Members.pm

--- C4/Members.pm
+++ C4/Members.pm
@@ -88,7 +88,7 @@ BEGIN {
     #Insert data
     push @EXPORT, qw(
         &AddMember
-    &AddMember_Auto
+        &AddMember_Auto
         &AddMember_Opac
     );
 
@@ -113,7 +113,7 @@ use C4::Members;
 
 =head1 DESCRIPTION
 
-This module contains routines for adding, modifying and deleting members/patrons/borrowers 
+This module contains routines for adding, modifying and deleting members/patrons/borrowers
 
 =head1 FUNCTIONS
 
@@ -155,7 +155,7 @@ The following will be set where applicable:
  $flags->{WAITING}->{message}       Message -- deprecated
  $flags->{WAITING}->{itemlist}      ref-to-array: list of available items
 
-=over 
+=over
 
 =item C<$flags-E<gt>{ODUES}-E<gt>{itemlist}> is a reference-to-array listing the
 overdue items. Its elements are references-to-hash, each describing an
@@ -171,7 +171,7 @@ fields from the reserves table of the Koha database.
 
 =back
 
-All the "message" fields that include language generated in this function are deprecated, 
+All the "message" fields that include language generated in this function are deprecated,
 because such strings belong properly in the display layer.
 
 The "message" field that comes from the DB is OK.
@@ -308,7 +308,7 @@ sub GetMember {
     my $dbh = C4::Context->dbh;
     my $select =
     q{SELECT borrowers.*, categories.category_type, categories.description
-    FROM borrowers 
+    FROM borrowers
     LEFT JOIN categories on borrowers.categorycode=categories.categorycode WHERE };
     my $more_p = 0;
     my @values = ();
@@ -655,7 +655,7 @@ sub fixup_cardnumber {
         my ($result) = $sth->fetchrow;
         return $result + 1;
     }
-    return $cardnumber;     # just here as a fallback/reminder 
+    return $cardnumber;     # just here as a fallback/reminder
 }
 
 =head2 GetPendingIssues
@@ -773,14 +773,14 @@ sub GetAllIssues {
     my $dbh = C4::Context->dbh;
     my $query =
 'SELECT *, issues.timestamp as issuestimestamp, issues.renewals AS renewals,items.renewals AS totalrenewals,items.timestamp AS itemstimestamp
-  FROM issues 
+  FROM issues
   LEFT JOIN items on items.itemnumber=issues.itemnumber
   LEFT JOIN biblio ON items.biblionumber=biblio.biblionumber
   LEFT JOIN biblioitems ON items.biblioitemnumber=biblioitems.biblioitemnumber
-  WHERE borrowernumber=? 
+  WHERE borrowernumber=?
   UNION ALL
-  SELECT *, old_issues.timestamp as issuestimestamp, old_issues.renewals AS renewals,items.renewals AS totalrenewals,items.timestamp AS itemstimestamp 
-  FROM old_issues 
+  SELECT *, old_issues.timestamp as issuestimestamp, old_issues.renewals AS renewals,items.renewals AS totalrenewals,items.timestamp AS itemstimestamp
+  FROM old_issues
   LEFT JOIN items on items.itemnumber=old_issues.itemnumber
   LEFT JOIN biblio ON items.biblionumber=biblio.biblionumber
   LEFT JOIN biblioitems ON items.biblioitemnumber=biblioitems.biblioitemnumber
@@ -816,8 +816,8 @@ sub GetMemberAccountRecords {
     my @acctlines;
     my $numlines = 0;
     my $strsth      = qq(
-                        SELECT * 
-                        FROM accountlines 
+                        SELECT *
+                        FROM accountlines
                         WHERE borrowernumber=?);
     $strsth.=" ORDER BY accountlines_id desc";
     my $sth= $dbh->prepare( $strsth );
@@ -895,11 +895,11 @@ sub GetBorNotifyAcctRecord {
     my @acctlines;
     my $numlines = 0;
     my $sth = $dbh->prepare(
-            "SELECT * 
-                FROM accountlines 
-                WHERE borrowernumber=? 
-                    AND notify_id=? 
-                    AND amountoutstanding != '0' 
+            "SELECT *
+                FROM accountlines
+                WHERE borrowernumber=?
+                    AND notify_id=?
+                    AND amountoutstanding != '0'
                 ORDER BY notify_id,accounttype
                 ");
 
@@ -982,8 +982,8 @@ sub get_cardnumber_length {
 
   $email = GetFirstValidEmailAddress($borrowernumber);
 
-Return the first valid email address for a borrower, given the borrowernumber.  For now, the order 
-is defined as email, emailpro, B_email.  Returns the empty string if the borrower has no email 
+Return the first valid email address for a borrower, given the borrowernumber.  For now, the order
+is defined as email, emailpro, B_email.  Returns the empty string if the borrower has no email
 addresses.
 
 =cut
@@ -1048,11 +1048,11 @@ sub GetBorrowersToExpunge {
     my $filtercategory   = $params->{'category_code'};
     my $filterbranch     = $params->{'branchcode'} ||
                         ((C4::Context->preference('IndependentBranches')
-                             && C4::Context->userenv 
+                             && C4::Context->userenv
                              && !C4::Context->IsSuperLibrarian()
                              && C4::Context->userenv->{branch})
                          ? C4::Context->userenv->{branch}
-                         : "");  
+                         : "");
     my $filterpatronlist = $params->{'patron_list_id'};
 
     my $dbh   = C4::Context->dbh;
@@ -1105,13 +1105,13 @@ sub GetBorrowersToExpunge {
     warn $query if $debug;
 
     my $sth = $dbh->prepare($query);
-    if (scalar(@query_params)>0){  
+    if (scalar(@query_params)>0){
         $sth->execute(@query_params);
     }
     else {
         $sth->execute;
     }
-    
+
     my @results;
     while ( my $data = $sth->fetchrow_hashref ) {
         push @results, $data;
