b210cb0dc83e7c490bd43c74e8d54d53f70e4118
Kyle M Hall <kyle@bywatersolutions.com>
2014-04-25 12:00:30 -0400

    MDAH - Bug 8352 - Add printer administration, update script to use Koha defined printers
    This patch adds the ability to define printers within Koha. The printer
    CRUD is based on the code for the quote of the day system.
    
    This patch extends the existing ability to print via LPR to any network
    printer with the added ability to print to a CUPS printer configured on
    the Koha server.
    
    Test Plan:
    1) Apply this patch set
    2) Run updatedatabase.pl
    3) Browse to admin/printers.pl
    4) Configure a network printer via LPR ( explained in the printers help )
    5) Create a hold
    6) Execute the command "misc/cronjobs/holds/print_holds.pl -v --printer $BRANCHCODE=$PRINTER_ID
       where branchcode is the pickup branch for the hold, and printer_id is
       the id number for the printer.
    7) See your reserve slip is printer!
    
    Bonus points: Repeat the test plan, but for a printer you set up via CUPS on the Koha server
    
    Conflicts:
    	admin/printers.pl
    	installer/data/mysql/updatedatabase.pl
    	koha-tmpl/intranet-tmpl/prog/en/includes/printers-admin-search.inc
    	koha-tmpl/intranet-tmpl/prog/en/modules/admin/printers.tt

--- Koha/Schema/Result/Printer.pm
+++ Koha/Schema/Result/Printer.pm
@@ -1,8 +1,13 @@
-use utf8;
 package Koha::Schema::Result::Printer;
 
 # Created by DBIx::Class::Schema::Loader
 # DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
 
 =head1 NAME
 
@@ -10,65 +15,106 @@ Koha::Schema::Result::Printer
 
 =cut
 
-use strict;
-use warnings;
-
-use base 'DBIx::Class::Core';
-
-=head1 TABLE: C<printers>
-
-=cut
-
 __PACKAGE__->table("printers");
 
 =head1 ACCESSORS
 
-=head2 printername
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 name
 
   data_type: 'varchar'
   default_value: (empty string)
   is_nullable: 0
   size: 40
 
-=head2 printqueue
+=head2 queue
 
   data_type: 'varchar'
   is_nullable: 1
   size: 20
 
-=head2 printtype
+=head2 type
 
-  data_type: 'varchar'
+  data_type: 'text'
   is_nullable: 1
-  size: 20
 
 =cut
 
 __PACKAGE__->add_columns(
-  "printername",
+  "id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "name",
   { data_type => "varchar", default_value => "", is_nullable => 0, size => 40 },
-  "printqueue",
+  "queue",
   { data_type => "varchar", is_nullable => 1, size => 20 },
-  "printtype",
-  { data_type => "varchar", is_nullable => 1, size => 20 },
+  "type",
+  { data_type => "text", is_nullable => 1 },
 );
-
-=head1 PRIMARY KEY
-
-=over 4
-
-=item * L</printername>
-
-=back
-
-=cut
-
-__PACKAGE__->set_primary_key("printername");
+__PACKAGE__->set_primary_key("id");
 
 
-# Created by DBIx::Class::Schema::Loader v0.07025 @ 2013-10-14 20:56:21
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:OaIxwtdwn0TJLggwOygC2w
+# Created by DBIx::Class::Schema::Loader v0.07000 @ 2014-05-19 09:04:19
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:uapO0jjBiwFJZjpbJMas/g
 
+sub print {
+    my ( $self, $params ) = @_;
 
-# You can replace this text with custom content, and it will be preserved on regeneration
+    my $data    = $params->{data};
+    my $is_html = $params->{is_html};
+
+    return unless ($data);
+
+    if ($is_html) {
+        require HTML::HTMLDoc;
+        my $htmldoc = new HTML::HTMLDoc();
+        $htmldoc->set_output_format('ps');
+        $htmldoc->set_html_content($data);
+        my $doc = $htmldoc->generate_pdf();
+        $data = $doc->to_string();
+    }
+
+    my ( $result, $error );
+
+    if ( $self->queue() =~ /:/ ) {
+
+        # Printer has a server:port address, use Net::Printer
+        require Net::Printer;
+
+        my ( $server, $port ) = split( /:/, $self->queue() );
+
+        my $printer = new Net::Printer(
+            printer     => $self->name(),
+            server      => $server,
+            port        => $port,
+            lineconvert => "YES"
+        );
+
+        $result = $printer->printstring($data);
+
+        $error = $printer->printerror();
+    }
+    else {
+        require Printer;
+
+        my $printer_name = $self->name();
+
+        my $printer = new Printer( 'linux' => 'lp' );
+        $printer->print_command(
+            linux => {
+                type    => 'pipe',
+                command => "lp -d $printer_name",
+            }
+        );
+
+        $result = $printer->print($data);
+    }
+
+    return ( $result eq '1', $error );
+}
+
 1;
