69efad4c6261633846c0a64621ff6cb258b96abd
Kyle M Hall <kyle@bywatersolutions.com>
2014-07-15 13:12:46 -0400

    BARCODE PREFIXES
    Allows for specification, on a per branch basis, of barcode prefixes for both items and patrons.
    Barcodes must come to a specific length (as set in system preferences), and barcodes shorter than
    this length will be auto-prefixed with the currently-logged-in branch's prefix.
    
    Currently supports:
    Circulation (checkout, return)
    Patron lookup
    Item creation
    Patron creation
    Bulk patron import
    Inventory
    Offline Circ
    ILSDI
    batch item modification/deletion tool
    Label creator
    SCO Module
    
    http://bugs.koha-community.org/show_bug.cgi?id=7676
    
    BARCODE PREFIXES - Template branch values are now stored in the 'library' variable instead of directly
    
    BARCODE PREFIXES - Fixing extraneous colon in branches.pl
    
    modified:   admin/branches.pl
    
    BARCODE PREFIXES - Fix bug introduced by rebase
    
    BARCODE PREFIXES - Fix issues introduces by rebase to Koha version 18.11
    
    BARCODE PREFIXES - Fix typo Koha::Patrons->new => Koha::Patron->new
    
    BARCODE PREFIXES - Simplify logic for fixup_cardnumber
    
    BARCODE PREFIXES - Update simplified logic, limit cardnumbers looked at to the same length as the patronbarcodelength syspref
    
    This prevents screwy cardnumbers from making the next cardnumber jump over a large swath of available cardnumbers
    
    BARCODE PREFIXES - Clean up C4::Members::_prefix_cardnum
    
    BARCODE PREFIXES - Fix issue with SCO
    
    BARCODE PREFIXES - Fix issue with SCO before searching for patron
    
    BARCODE PREFIXES - Prefix before checking cardnumber for duplicates
    
    BARCODE PREFIXES - Fix minor syntax error, forgot to declare
    
    BARCODE PREFIXES - Update code for REST API

--- circ/returns.pl
+++ circ/returns.pl
@@ -115,7 +115,8 @@ foreach ( $query->param ) {
 
     # decode barcode    ## Didn't we already decode them before passing them back last time??
     $barcode =~ s/^\s*|\s*$//g; # remove leading/trailing whitespace
-    $barcode = barcodedecode($barcode) if(C4::Context->preference('itemBarcodeInputFilter'));
+    $barcode = barcodedecode($barcode) if(C4::Context->preference('itemBarcodeInputFilter')
+                                          || C4::Context->preference('itembarcodelength'));
 
     ######################
     #Are these lines still useful ?
@@ -249,7 +250,7 @@ if ($transit) {
 my $returnbranch;
 if ($barcode) {
     $barcode =~ s/^\s*|\s*$//g; # remove leading/trailing whitespace
-    $barcode = barcodedecode($barcode) if C4::Context->preference('itemBarcodeInputFilter');
+    $barcode = barcodedecode($barcode) if C4::Context->preference('itemBarcodeInputFilter') || C4::Context->preference('itembarcodelength');
     my $item = Koha::Items->find({ barcode => $barcode });
 
     if ( $item ) {
