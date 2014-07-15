b019cf3a532284b1f130f86350b77907034c7d5a
Kyle M Hall <kyle@bywatersolutions.com>
2014-07-15 13:12:46 -0400

    BARCODE PREFIXES
    Allows for specification, on a per branch basis, of barcode prefixes for both items and patrons.
    Barcodes must come to a specific length (as set in system preferences), and barcodes shorter than
    this length will be auto-prefixed with the currently-logged-in branch's prefix.
    
    Currently supports:
    Circulation (checkout, return)
    Patron lookup
    Item creation
    Patron creation
    Bulk patron import
    Inventory
    Offline Circ
    ILSDI
    batch item modification/deletion tool
    Label creator
    SCO Module
    
    http://bugs.koha-community.org/show_bug.cgi?id=7676
    
    BARCODE PREFIXES - Template branch values are now stored in the 'library' variable instead of directly
    
    BARCODE PREFIXES - Fixing extraneous colon in branches.pl
    
    modified:   admin/branches.pl
    
    BARCODE PREFIXES - Fix bug introduced by rebase
    
    BARCODE PREFIXES - Fix issues introduces by rebase to Koha version 18.11
    
    BARCODE PREFIXES - Fix typo Koha::Patrons->new => Koha::Patron->new
    
    BARCODE PREFIXES - Simplify logic for fixup_cardnumber
    
    BARCODE PREFIXES - Update simplified logic, limit cardnumbers looked at to the same length as the patronbarcodelength syspref
    
    This prevents screwy cardnumbers from making the next cardnumber jump over a large swath of available cardnumbers
    
    BARCODE PREFIXES - Clean up C4::Members::_prefix_cardnum
    
    BARCODE PREFIXES - Fix issue with SCO
    
    BARCODE PREFIXES - Fix issue with SCO before searching for patron
    
    BARCODE PREFIXES - Prefix before checking cardnumber for duplicates
    
    BARCODE PREFIXES - Fix minor syntax error, forgot to declare
    
    BARCODE PREFIXES - Update code for REST API

--- C4/SIP/Sip/MsgType.pm
+++ C4/SIP/Sip/MsgType.pm
@@ -17,6 +17,7 @@ use C4::SIP::Sip::Checksum qw(verify_cksum);
 use Data::Dumper;
 use CGI qw ( -utf8 );
 use C4::Auth qw(&check_api_auth);
+use C4::Members; # qw(_prefix_cardnum)
 
 use Koha::Patrons;
 use Koha::Patron::Attributes;
@@ -522,7 +523,11 @@ sub handle_checkout {
     $fields = $self->{fields};
 
     $patron_id = $fields->{ (FID_PATRON_ID) };
+
     Koha::Plugins->call('patron_barcode_transform', \$patron_id );
+
+    $patron_id = C4::Members::_prefix_cardnum( $patron_id );
+
     $item_id   = $fields->{ (FID_ITEM_ID) };
     my $fee_ack = $fields->{ (FID_FEE_ACK) };
 
@@ -747,7 +752,7 @@ sub handle_block_patron {
     $fields           = $self->{fields};
     $inst_id          = $fields->{ (FID_INST_ID) };
     $blocked_card_msg = $fields->{ (FID_BLOCKED_CARD_MSG) };
-    $patron_id        = $fields->{ (FID_PATRON_ID) };
+    $patron_id        = C4::Members::_prefix_cardnum( $fields->{(FID_PATRON_ID)} );
     $terminal_pwd     = $fields->{ (FID_TERMINAL_PWD) };
 
     Koha::Plugins->call('patron_barcode_transform', \$patron_id );
@@ -970,7 +975,7 @@ sub handle_patron_info {
     my ( $resp, $patron );
 
     $inst_id      = $fields->{ (FID_INST_ID) };
-    $patron_id    = $fields->{ (FID_PATRON_ID) };
+    $patron_id    = C4::Members::_prefix_cardnum( $fields->{(FID_PATRON_ID)} );
     $terminal_pwd = $fields->{ (FID_TERMINAL_PWD) };
     $patron_pwd   = $fields->{ (FID_PATRON_PWD) };
     $start        = $fields->{ (FID_START_ITEM) };
@@ -1130,7 +1135,7 @@ sub handle_fee_paid {
 
     $fee_amt    = $fields->{ (FID_FEE_AMT) };
     $inst_id    = $fields->{ (FID_INST_ID) };
-    $patron_id  = $fields->{ (FID_PATRON_ID) };
+    $patron_id  = C4::Members::_prefix_cardnum( $fields->{(FID_PATRON_ID)} );
     $patron_pwd = $fields->{ (FID_PATRON_PWD) };
     $fee_id     = $fields->{ (FID_FEE_ID) };
     $trans_id   = $fields->{ (FID_TRANSACTION_ID) };
@@ -1336,7 +1341,7 @@ sub handle_patron_enable {
     my $resp = PATRON_ENABLE_RESP;
 
     ($trans_date) = @{ $self->{fixed_fields} };
-    $patron_id  = $fields->{ (FID_PATRON_ID) };
+    $patron_id  = C4::Members::_prefix_cardnum( $fields->{(FID_PATRON_ID)} );
     $patron_pwd = $fields->{ (FID_PATRON_PWD) };
 
     Koha::Plugins->call('patron_barcode_transform', \$patron_id );
@@ -1395,7 +1400,7 @@ sub handle_hold {
 
     $ils->check_inst_id( $fields->{ (FID_INST_ID) }, "handle_hold" );
 
-    $patron_id   = $fields->{ (FID_PATRON_ID) };
+    $patron_id   = C4::Members::_prefix_cardnum( $fields->{(FID_PATRON_ID)} );
     $expiry_date = $fields->{ (FID_EXPIRATION) } || '';
     $pickup_locn = $fields->{ (FID_PICKUP_LOCN) } || '';
     $hold_type   = $fields->{ (FID_HOLD_TYPE) } || '2';    # Any copy of title
@@ -1464,7 +1469,7 @@ sub handle_renew {
         siplog( "LOG_WARNING", "handle_renew: received 'no block' renewal from terminal '%s'", $server->{account}->{id} );
     }
 
-    $patron_id  = $fields->{ (FID_PATRON_ID) };
+    $patron_id  = C4::Members::_prefix_cardnum( $fields->{(FID_PATRON_ID)} );
     $patron_pwd = $fields->{ (FID_PATRON_PWD) };
     $item_id    = $fields->{ (FID_ITEM_ID) };
     $title_id   = $fields->{ (FID_TITLE_ID) };
@@ -1549,7 +1554,7 @@ sub handle_renew_all {
 
     ($trans_date) = @{ $self->{fixed_fields} };
 
-    $patron_id    = $fields->{ (FID_PATRON_ID) };
+    $patron_id    = C4::Members::_prefix_cardnum( $fields->{(FID_PATRON_ID)} );
     $patron_pwd   = $fields->{ (FID_PATRON_PWD) };
     $terminal_pwd = $fields->{ (FID_TERMINAL_PWD) };
     $fee_ack      = $fields->{ (FID_FEE_ACK) };
