86ad8a1d3c5e142d3a3b0fcaec188facf83ed37b
Kyle M Hall <kyle@bywatersolutions.com>
2014-07-15 13:12:46 -0400

    BARCODE PREFIXES
    Allows for specification, on a per branch basis, of barcode prefixes for both items and patrons.
    Barcodes must come to a specific length (as set in system preferences), and barcodes shorter than
    this length will be auto-prefixed with the currently-logged-in branch's prefix.
    
    Currently supports:
    Circulation (checkout, return)
    Patron lookup
    Item creation
    Patron creation
    Bulk patron import
    Inventory
    Offline Circ
    ILSDI
    batch item modification/deletion tool
    Label creator
    SCO Module
    
    http://bugs.koha-community.org/show_bug.cgi?id=7676
    
    BARCODE PREFIXES - Template branch values are now stored in the 'library' variable instead of directly
    
    BARCODE PREFIXES - Fixing extraneous colon in branches.pl
    
    modified:   admin/branches.pl
    
    BARCODE PREFIXES - Fix bug introduced by rebase
    
    BARCODE PREFIXES - Fix issues introduces by rebase to Koha version 18.11
    
    BARCODE PREFIXES - Fix typo Koha::Patrons->new => Koha::Patron->new
    
    BARCODE PREFIXES - Simplify logic for fixup_cardnumber
    
    BARCODE PREFIXES - Update simplified logic, limit cardnumbers looked at to the same length as the patronbarcodelength syspref
    
    This prevents screwy cardnumbers from making the next cardnumber jump over a large swath of available cardnumbers
    
    BARCODE PREFIXES - Clean up C4::Members::_prefix_cardnum
    
    BARCODE PREFIXES - Fix issue with SCO
    
    BARCODE PREFIXES - Fix issue with SCO before searching for patron
    
    BARCODE PREFIXES - Prefix before checking cardnumber for duplicates
    
    BARCODE PREFIXES - Fix minor syntax error, forgot to declare
    
    BARCODE PREFIXES - Update code for REST API

--- C4/Circulation.pm
+++ C4/Circulation.pm
@@ -166,8 +166,11 @@ sub barcodedecode {
     my ($barcode, $filter) = @_;
     my $branch = C4::Context::mybranch();
     $filter = C4::Context->preference('itemBarcodeInputFilter') unless $filter;
+
     Koha::Plugins->call('item_barcode_transform',  \$barcode );
-    $filter or return $barcode;     # ensure filter is defined, else return untouched barcode
+
+    return $barcode unless ( $filter || C4::Context->preference('itembarcodelength') ); # ensure filter is defined, else return untouched barcode
+
 	if ($filter eq 'whitespace') {
 		$barcode =~ s/\s//g;
 	} elsif ($filter eq 'cuecat') {
@@ -201,6 +204,11 @@ sub barcodedecode {
             warn "# [$barcode] not valid EAN-13/UPC-A\n";
         }
 	}
+        if(C4::Context->preference('itembarcodelength') && (length($barcode) < C4::Context->preference('itembarcodelength'))) {
+                my $prefix = Koha::Libraries->find( C4::Context->userenv->{'branch'} )->itembarcodeprefix;
+                my $padding = C4::Context->preference('itembarcodelength') - length($prefix) - length($barcode) ;
+                $barcode = $prefix . '0' x $padding . $barcode if($padding >= 0) ;
+        }
     return $barcode;    # return barcode, modified or not
 }
 
