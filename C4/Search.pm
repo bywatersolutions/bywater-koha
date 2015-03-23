b7719fbe8c648c41b3a9b043074c70d70615b247
Barton Chittenden <barton@bywatersolutions.com>
2015-03-23 15:45:09 -0400

    SWITCH - Add index Title-key:p to marc 222

--- C4/Search.pm
+++ C4/Search.pm
@@ -1238,6 +1238,7 @@ sub getIndexes{
                     'ti',
                     'Title',
                     'Title-cover',
+                    'Title-key',
                     'Title-series',
                     'Title-uniform',
                     'Title-uniform-heading',
