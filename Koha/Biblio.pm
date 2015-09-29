4ad8f661147af55dc78844dfa75bf5a912a369cc
Kyle M Hall <kyle@bywatersolutions.com>
2015-09-29 13:24:29 -0400

    BWS-PKG - Bug 14694 - Make decreaseloanHighHolds more flexible
    This patch allows for more flexibility for determining when the number
    of holds a record has should trigger the reduction of the loan length
    for items on that record.
    
    This patch adds a new system preference decreaseLoanHighHoldsControl,
    which defaults to 'static', the original behavior of the feature.
    It also has a new behavior 'dynamic' which makes the feature only
    decrease the loan length if the number of holds on the record exceeds
    the number of holdable items + decreaseLoanHighHoldsValue.
    
    It also allows items to be filtered from the list of items based
    on the damaged, lost, not for loan, and withdrawn values even if
    those values would have allowed holds ( i.e. values < 0 )
    
    Test Plan:
    1) Apply this patch
    2) Run updatedatabase.pl
    3) Set decreaseLoanHighHolds to Enable
    4) Set decreaseLoanHighHoldsControl to "over the number of items on the record"
    5) Set decreaseLoanHighHoldsDuration to 1
    6) Set decreaseLoanHighHoldsValue to 3
    7) Create a record with 5 items
    8) Please 8 or more holds on the record
    9) Check out one of the items to a patron
    10) Note the loan length is reduced to 1 day
    11) Set decreaseLoanHighHoldsValue to 3 to 2
    12) Check out one of the items to a patron
    13) Note the loan length is *not* reduced
    14) Enbale all the filters possible in decreaseLoanHighHoldsIgnoreStatuses
    15) Set one item to be damaged
    16) Note the loan length is reduced
    17) Unset the damaged status
    18) Repeat steps 15 - 17 for lost, not for loan, and withdrawn
    
    Signed-off-by: Christopher Brannon <cbrannon@cdalibrary.org>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Bug 14694 [QA Followup] - Fix unit tests
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Bug 14694 [QA Followup] - Fix typos
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Bug 14694 [QA Followup] - Fix syspref order
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Bug 14694: Create data the tests need
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- Koha/Biblio.pm
+++ Koha/Biblio.pm
@@ -35,6 +35,37 @@ Koha::Biblio - Koha Biblio Object class
 
 =cut
 
+=head3 subtitles
+
+my @subtitles = $biblio->subtitles();
+
+Returns list of subtitles for a record.
+
+Keyword to MARC mapping for subtitle must be set for this method to return any possible values.
+
+=cut
+
+sub subtitles {
+    my ( $self ) = @_;
+
+    return map { $_->{subfield} } @{ GetRecordValue( 'subtitle', GetMarcBiblio( $self->id ), $self->frameworkcode ) };
+}
+
+=head3 items
+
+Returns the related Koha::Items object for this biblio in scalar context,
+or list of Koha::Item objects in list context.
+
+=cut
+
+sub items {
+    my ($self) = @_;
+
+    $self->{_items} ||= Koha::Items->search( { biblionumber => $self->biblionumber() } );
+
+    return wantarray ? $self->{_items}->as_list : $self->{_items};
+}
+
 =head3 type
 
 =cut
