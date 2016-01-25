acd789ddff30f79fa574749168d9978461299a9b
Barton Chittenden <barton@bywatersolutions.com>
2016-01-25 14:05:25 -0800

    PACIFICA - BWS Custom: rt 31109 Add zebra index Theme
    Add zebra index 'Theme' for 690 subfield a

--- C4/Search.pm
+++ C4/Search.pm
@@ -1234,6 +1234,7 @@ sub getIndexes{
                     'Term-genre-form-heading',
                     'Term-genre-form-see',
                     'Term-genre-form-seealso',
+                    'Theme',
                     'ti',
                     'Title',
                     'Title-cover',
