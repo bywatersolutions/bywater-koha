1800d58a6c7a6a695d412bbfade70dff2e9c03d8
Jesse Weaver <jweaver@bywatersolutions.com>
2016-01-28 15:01:24 -0700

    SDLAW - Bug 15685: Allow creation of items (AcqCreateItem) to be customizable per-basket
    This adds a new basket attribute (create_items) that can optionally be
    set to override AcqCreateItem.
    
    The following have been modified to reflect this (with the value of
    create_items that causes them to behave differently in parentheses):
      * Cancelling receipt of an order (receiving)
      * Creating an order by hand or from MARC (ordering)
      * Receiving an order (receiving)
      * Showing orders with uncertain price (ordering)
      * Showing orders (receiving)
      * Showing acquisition details in the OPAC (ordering)
    
    Test plan:
      1) Create baskets with "Create items when:" set to ordering,
         receiving, cataloging and unset.
      2) Test each of the above for each of these baskets, verifying that
         the basket-specific attribute overrides AcqCreateItem if set and
         falls back to the syspref otherwise.
    
    NOTE: A check of AcqCreateItem in opac-detail.tt was removed because it
    was redundant; the code path in question cannot be triggered unless
    create_items/AcqCreateItems is set to the correct value anyway.
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Barbara Fondren <bfondren@roundrocktexas.gov>

--- acqui/basketheader.pl
+++ acqui/basketheader.pl
@@ -154,6 +154,7 @@ if ( $op eq 'add_form' ) {
             $input->param('deliveryplace'),
             $input->param('billingplace'),
             $input->param('is_standing') ? 1 : undef,
+            $input->param('create_items'),
         );
     } else { #New basket
         $basketno = NewBasket(
@@ -166,6 +167,7 @@ if ( $op eq 'add_form' ) {
             $input->param('deliveryplace'),
             $input->param('billingplace'),
             $input->param('is_standing') ? 1 : undef,
+            $input->param('create_items'),
         );
     }
 
