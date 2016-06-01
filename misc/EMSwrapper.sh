4534436597b4e6484d279e7ef7511eb3298013e7
Kyle M Hall <kyle@bywatersolutions.com>
2016-06-01 15:24:58 +0000

    MARYWOOD - Dematic module - Ticket #34488
    MARYWOOD - Update email addresses for EMSServer
    
    MARYWOOD - More updates to Dematic / EMS Server
    
    MARYWOOD - More updates to Dematic / EMS Server - Ticket 36660
    
    MARYWOOD - Bug 17215 - Autmember num with ldap
    
    Added per rt 37246
    
    MARYWOOD - Committing change to Auth_with_ldap
    
    This change was made in Sept 2016, but I don't know by whom. Seems to
    be a legitimate change, and not just debugging, so committing it now.
    --lrb
    
    modified:   C4/Auth_with_ldap.pm
    
    MARYWOOD - Don't load EMSServer during 00-load.t
    
    MARYWOOD - let t/Circulation_barcodedecode.t pass
    
    MARYWOOD - Update EMSServer launcher

---
+++ misc/EMSwrapper.sh
@@ -0,0 +1,8 @@
+# Executes the actual Perl script
+# The '&' puts the process into the background (as a daemon)
+# EMSServer.pm acts as a socket server to accept messages
+# from Marywood's EMS server
+
+## Change path as necessary to file's actual location
+perl /usr/share/koha/lib/C4/EMSServer.pm & # ampersand used to initiate module as a background process
+#perl /home/koha/kohaclone/C4/EMSServer.pm &
