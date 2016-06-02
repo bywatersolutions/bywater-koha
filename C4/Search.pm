7fff1b35ab269831454e0d71729a0bcf39820d74
Larry Baerveldt <larry@bywatersolutions.com>
2016-06-02 16:06:47 -0700

    BWS-PKG - Bug XXX - Index for 856$z, Electronic-Copy-Availability
    Originally created for Guggenheim, per RT 6907, by KMH, 20130114
    Committed for package by LRB. 20141028
    Rebased for 3.18.05.1 by LRB. 20150417
    Rebased for 3.22.06 by LRB. 20160602
    
    modified:   C4/Search.pm
    modified:   etc/zebradb/ccl.properties
    modified:   etc/zebradb/marc_defs/marc21/biblios/biblio-koha-indexdefs.xml
    modified:   etc/zebradb/marc_defs/marc21/biblios/biblio-zebra-indexdefs.xsl

--- C4/Search.pm
+++ C4/Search.pm
@@ -1126,6 +1126,9 @@ sub getIndexes{
                     'diss',
                     'dtlm',
                     'EAN',
+# BWS Custom for Guggenheim
+                    'Electronic-Copy-Availability',
+                    'eca',
                     'extent',
                     'fic',
                     'fiction',
