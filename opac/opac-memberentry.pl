c969dc0c30150509f0fad9d16526f4e5ff2bcd20
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2016-10-27 10:04:30 +0200

    Bug 17494: Make sure the same verification token won't be generated twice
    Well, this patch does not enforce the unique constraint but mimic what
    is already done in C4::ImportExportFramework and Koha::Upload where
    md5_hex is used.
    
    Test plan:
    Have a look at the code and confirm that it adds more randomness to the
    hashed string
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@biblibre.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- opac/opac-memberentry.pl
+++ opac/opac-memberentry.pl
@@ -123,8 +123,9 @@ if ( $action eq 'create' ) {
             );
             $template->param( 'email' => $borrower{'email'} );
 
-            my $verification_token = md5_hex( \%borrower );
             $borrower{'password'} = random_string("..........");
+
+            my $verification_token = md5_hex( time().{}.rand().{}.$$ );
 
             Koha::Patron::Modifications->new(
                 verification_token => $verification_token )
