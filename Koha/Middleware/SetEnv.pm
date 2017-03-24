e37f94f75e1bead7d12c71707b6052e46995f9e8
Larry Baerveldt <larry@bywatersolutions.com>
2017-03-24 12:41:17 -0700

    BWS-PKG - Adding custom ProxyPass and allow SetEnv options to plack configs
    Added additional ProxyPass settings to apache-shared files, and added
    support for custom SetEnv variables.
    
    modified:   Koha/Middleware/SetEnv.pm
    modified:   debian/templates/apache-shared-intranet-plack.conf

--- Koha/Middleware/SetEnv.pm
+++ Koha/Middleware/SetEnv.pm
@@ -64,12 +64,15 @@ name.
 =cut
 
 my $allowed_setenvs = qr/^(
+    MEMCACHED_SERVERS |
+    MEMCACHED_NAMESPACE |
     OVERRIDE_SYSPREF_(\w+) |
     OVERRIDE_SYSPREF_NAMES |
     OPAC_CSS_OVERRIDE |
     OPAC_SEARCH_LIMIT |
     OPAC_LIMIT_OVERRIDE |
-    TZ
+    TZ |
+    TMPDIR
 )\ /x;
 
 sub call {
