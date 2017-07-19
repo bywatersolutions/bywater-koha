bywatersolutions/koha-archived

ROUND ROCK - Bug 16899 - Add ability to disallow overpayments
Some SIP services ( such as Comprise ) require that an attempt at
over-paying a patron's account via SIP2 should fail, rather than create
a credit on the account. We should make this a configurable option on a
per-login basis in the SIP2 config file.

Test Plan:
1) Apply this patch
2) Enable the new parameter
   disallow_overpayment="1"
   for the login to be used in this test.
3) Restart your SIP server
4) Create or find a patron with fines
5) Attempt to send a payment via SIP for more than what the
   patron's balance is
6) Note the response indicates a payment failure
7) Attempt to send a payment via SIP for the account balance or
   less
8) Note the response indicates the payment has succeeded
9) Verify in Koha that the payment was processed

C4/SIP/ILS/Transaction/FeePayment.pm
@@ -21,7 +21,7 @@ use strict;
 # along with Koha; if not, see <http://www.gnu.org/licenses>.
 
 use C4::Accounts qw(recordpayment makepayment WriteOffFee);
-use Koha::Account::Lines;
+use Koha::Borrowers;
 use parent qw(C4::SIP::ILS::Transaction);
 
 
@@ -49,8 +49,17 @@ sub pay {
     my $type           = shift;
     my $fee_id         = shift;
     my $is_writeoff    = shift;
+    my $disallow_overpayment = shift;
 
     warn("RECORD:$borrowernumber::$amt");
+
+    if ($disallow_overpayment) {
+        my $patron = Koha::Borrowers->find($borrowernumber);
+        return 0 unless $patron;
+
+        my $balance = $patron->account_balance();
+        return 0 if $balance < $amt;
+    }
 
     my $fee = $fee_id ? Koha::Account::Lines->find($fee_id) : undef;
 

bywatersolutions/bywater-koha

C4/SIP/ILS/Transaction/FeePayment.pm
@@ -23,6 +23,7 @@ use strict;
 use Koha::Account;
 use C4::Accounts qw(makepayment WriteOffFee);
 use Koha::Account::Lines;
+use Koha::Patrons;
 use parent qw(C4::SIP::ILS::Transaction);
 
 
@@ -49,8 +50,17 @@ sub pay {
     my $type           = shift;
     my $fee_id         = shift;
     my $is_writeoff    = shift;
+    my $disallow_overpayment = shift;
 
     warn("RECORD:$borrowernumber::$amt");
+
+    if ($disallow_overpayment) {
+        my $patron = Koha::Patrons->find($borrowernumber);
+        return 0 unless $patron;
+
+        my $balance = $patron->account_balance();
+        return 0 if $balance < $amt;
+    }
 
     my $fee = $fee_id ? Koha::Account::Lines->find($fee_id) : undef;
 