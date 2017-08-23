a344bf52469d4a7521609f1f4588c95de4e2935c
Nick Clemens <nick@bywatersolutions.com>
2017-08-23 16:13:39 +0000

    VATECH - Bug 19166 - Add the ability to add adjustments to an invoice
    This patchset adds the ability to add adjustments to an invoice, one can
    provide a reason, an adjustment amount, select a budget, and choose
    whether to encumber the funds before the invoice is closed or not
    
    To test:
    
    1 - Create a new invoice with or without a shipping cost
    2 - Note there are no existing adjustments
    3 - Add an adjustment
    4 - Submit the form withno changes, nothing happens
    5 - Update the adjustment you created, ensure changes are saved but no
    extra adjustment created
    6 - Add another invoice prodiving only reason or amount (you can have 0
            value adjustments)
    7 - Verify the adjustment total at bottom is correct
    8 - Recieve some orders
    9 - Verify totals are correct
    
    Conflicts:
    	C4/Budgets.pm

--- acqui/spent.pl
+++ acqui/spent.pl
@@ -35,6 +35,7 @@ use C4::Output;
 use strict;
 use warnings;
 use CGI qw ( -utf8 );
+use Koha::InvoiceAdjustments;
 
 my $dbh      = C4::Context->dbh;
 my $input    = new CGI;
@@ -119,6 +120,11 @@ while (my $data = $sth->fetchrow_hashref) {
 }
 $sth->finish;
 
+my $adjustments = Koha::InvoiceAdjustments->search({budget_id => $bookfund, closedate => { '!=' => undef } }, { join => 'invoiceid' } );
+while ( my $adj = $adjustments->next ){
+    $total += $adj->adjustment;
+}
+
 $total = sprintf( "%.2f", $total );
 
 $template->param(
@@ -126,6 +132,7 @@ $template->param(
     spent => \@spent,
     subtotal => $subtotal,
     shipmentcosts => \@shipmentcosts,
+    adjustments => $adjustments,
     total => $total,
     fund_code => $fund_code
 );
