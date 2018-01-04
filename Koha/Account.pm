8164aceaefdbf438794fcfeb6f6bee67f8f2bf64
Kyle M Hall <kyle@bywatersolutions.com>
2018-01-04 09:24:59 -0500

    BWS-PKG - Restore the storing of itemnumber for writeoffs

--- Koha/Account.pm
+++ Koha/Account.pm
@@ -75,6 +75,7 @@ sub pay {
     my $type         = $params->{type} || 'payment';
     my $account_type = $params->{account_type};
     my $offset_type  = $params->{offset_type} || $type eq 'writeoff' ? 'Writeoff' : 'Payment';
+    my $itemnumber   = $params->{itemnumber} || undef;
 
     my $userenv = C4::Context->userenv;
 
@@ -219,6 +220,7 @@ sub pay {
             amountoutstanding => 0 - $balance_remaining,
             manager_id        => $manager_id,
             note              => $note,
+            itemnumber        => $itemnumber,
         }
     )->store();
 
