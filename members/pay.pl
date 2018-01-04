884aee43662a54e8499b00268e8f586e0527d085
Kyle M Hall <kyle@bywatersolutions.com>
2018-01-04 09:24:59 -0500

    BWS-PKG - Restore the storing of itemnumber for writeoffs

--- members/pay.pl
+++ members/pay.pl
@@ -94,6 +94,7 @@ elsif ( $input->param('confirm_writeoff') ) {
     my $accountlines_id = $input->param('accountlines_id');
     my $amount          = $input->param('amountwrittenoff');
     my $payment_note    = $input->param("payment_note");
+    my $itemno          = $input->param('itemnumber');
 
     my $accountline = Koha::Account::Lines->find( $accountlines_id );
 
@@ -115,6 +116,7 @@ elsif ( $input->param('confirm_writeoff') ) {
                 type       => 'writeoff',
                 note       => $payment_note,
                 library_id => $branch,
+                itemnumber => $itemno,
             }
         );
     }
@@ -219,6 +221,7 @@ sub writeoff_all {
             my $amount          = $input->param("amountoutstanding$value");
             my $accountlines_id = $input->param("accountlines_id$value");
             my $payment_note    = $input->param("payment_note_$value");
+            my $itemno          = $input->param("itemnumber$value");
             Koha::Account->new( { patron_id => $borrowernumber } )->pay(
                 {
                     amount => $amount,
@@ -226,6 +229,7 @@ sub writeoff_all {
                     type   => 'writeoff',
                     note   => $payment_note,
                     library_id => $branch,
+                    itemnumber => $itemno,
                 }
             );
         }
