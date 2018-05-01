45696c9d845411c20f2637bd85490f85e601f622
Kyle M Hall <kyle@bywatersolutions.com>
2018-05-01 15:49:01 +0000

    Bug 14570 [QA Followup]: Show add guarantor pane for correct patron types, fix "Add child" button

--- members/memberentry.pl
+++ members/memberentry.pl
@@ -744,7 +744,7 @@ if (C4::Context->preference('EnhancedMessagingPreferences')) {
     $template->param(TalkingTechItivaPhone => C4::Context->preference("TalkingTechItivaPhoneNotification"));
 }
 
-$template->param( "show_guarantor" => ( $category_type =~ /A|I|S|X/ ) ? 1 : 0 ); # associate with step to know where you are
+$template->param( "show_guarantor" => ( $category_type =~ /A|I|S|X/ ) ? 0 : 1 ); # associate with step to know where you are
 $debug and warn "memberentry step: $step";
 $template->param(%data);
 $template->param( "step_$step"  => 1) if $step;	# associate with step to know where u are
