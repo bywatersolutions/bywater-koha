4e66107ee68d9b990020dce946cf44240be83f59
Kyle M Hall <kyle@bywatersolutiosn.com>
2018-08-20 10:48:07 -0400

    CLIC - Bug 7614: Don't allow un-usable pickup locations in the opac pickup locations pulldown
    Also fully qualifies some subroutine calls that fail for reasons unkown.

--- opac/opac-reserve.pl
+++ opac/opac-reserve.pl
@@ -173,6 +173,7 @@ foreach my $biblioNumber (@biblionumbers) {
 
     # Compute the priority rank.
     my $biblio = Koha::Biblios->find( $biblioNumber );
+    $biblioData->{object} = $biblio;
     my $holds = $biblio->holds;
     my $rank = $holds->count;
     $biblioData->{reservecount} = 1;    # new reserve
@@ -389,7 +390,6 @@ $template->param('item_level_itypes' => $itemLevelTypes);
 
 foreach my $biblioNum (@biblionumbers) {
 
-    my @not_available_at = ();
     my $record = GetMarcBiblio({ biblionumber => $biblioNum });
     # Init the bib item with the choices for branch pickup
     my %biblioLoopIter;
@@ -399,6 +399,12 @@ foreach my $biblioNum (@biblionumbers) {
     if (! $biblioData) {
         $template->param(message=>1, bad_biblionumber=>$biblioNum);
         &get_out($query, $cookie, $template->output);
+    }
+
+    my @not_available_at = ();
+    my $biblio = $biblioData->{object};
+    foreach my $library ( $pickup_locations->as_list ) {
+        push( @not_available_at, $library->branchcode ) unless $biblio->can_be_transferred({ to => $library });
     }
 
     my $frameworkcode = GetFrameworkCode( $biblioData->{biblionumber} );
