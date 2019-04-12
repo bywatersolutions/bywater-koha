144ac1f5b2a81f8c2a7dec77d4106bb3bd7496ba
Kyle M Hall <kyle@bywatersolutions.com>
2019-04-12 09:39:52 -0400

    BWS-PKG - Fix bug introduced by rebase

--- Koha/Patron.pm
+++ Koha/Patron.pm
@@ -380,7 +380,7 @@ sub guarantor {
 
     return unless $self->guarantorid();
 
-    return Koha::Patrons->find( $self->guarantorid() );
+    return scalar Koha::Patrons->find( $self->guarantorid() );
 }
 
 sub image {
