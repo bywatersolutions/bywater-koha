7d64c0bfd36cfa2cd7789b00339d0c5d42768e10
Kyle M Hall <kyle@bywatersolutions.com>
2019-06-27 12:12:46 -0400

    PASCO - Bug 23228: Add option to automatically display payment receipt for printing after making a payment
    Some libraries would like an auto-popup after making a payment so librarians don't have
    to navigate to the accounts page, locate the new payment, then click the print button.
    
    Test Plan:
    1) Apply this patch
    2) Run updatedatabase.pl
    3) Make a payment, note no difference in behavior
    4) Enable the new syspref FinePaymentAutoPopup
    5) Make a payment, note the popup for the payment receipt

--- members/paycollect.pl
+++ members/paycollect.pl
@@ -36,8 +36,9 @@ use Koha::Token;
 
 my $input = CGI->new();
 
-my $writeoff_individual       = $input->param('writeoff_individual');
-my $type                      = scalar $input->param('type') || 'payment';
+my $payment_id          = $input->param('payment_id');
+my $writeoff_individual = $input->param('writeoff_individual');
+my $type                = scalar $input->param('type') || 'payment';
 
 my $updatecharges_permissions = ($writeoff_individual || $type eq 'writeoff') ? 'writeoff' : 'remaining_permissions';
 my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
@@ -121,7 +122,7 @@ if ( $total_paid and $total_paid ne '0.00' ) {
 
         if ($pay_individual) {
             my $line = Koha::Account::Lines->find($accountlines_id);
-            Koha::Account->new( { patron_id => $borrowernumber } )->pay(
+            $payment_id = Koha::Account->new( { patron_id => $borrowernumber } )->pay(
                 {
                     lines        => [$line],
                     amount       => $total_paid,
@@ -132,7 +133,7 @@ if ( $total_paid and $total_paid ne '0.00' ) {
                 }
             );
             print $input->redirect(
-                "/cgi-bin/koha/members/pay.pl?borrowernumber=$borrowernumber");
+                "/cgi-bin/koha/members/pay.pl?borrowernumber=$borrowernumber&payment_id=$payment_id");
         } else {
             if ($select) {
                 if ( $select =~ /^([\d,]*).*/ ) {
@@ -150,7 +151,7 @@ if ( $total_paid and $total_paid ne '0.00' ) {
                     { order_by => 'date' }
                 );
 
-                Koha::Account->new(
+                $payment_id = Koha::Account->new(
                     {
                         patron_id => $borrowernumber,
                     }
@@ -168,7 +169,7 @@ if ( $total_paid and $total_paid ne '0.00' ) {
             }
             else {
                 my $note = $input->param('selected_accts_notes');
-                Koha::Account->new( { patron_id => $borrowernumber } )->pay(
+                $payment_id = Koha::Account->new( { patron_id => $borrowernumber } )->pay(
                     {
                         amount       => $total_paid,
                         library_id   => $branch,
@@ -179,7 +180,7 @@ if ( $total_paid and $total_paid ne '0.00' ) {
                 );
             }
 
-            print $input->redirect("/cgi-bin/koha/members/boraccount.pl?borrowernumber=$borrowernumber");
+            print $input->redirect("/cgi-bin/koha/members/boraccount.pl?borrowernumber=$borrowernumber&payment_id=$payment_id");
         }
     }
 } else {
@@ -193,12 +194,14 @@ if ( $input->param('error_over') ) {
 }
 
 $template->param(
+    payment_id => $payment_id,
+
     type           => $type,
     borrowernumber => $borrowernumber,    # some templates require global
     patron        => $patron,
     total         => $total_due,
 
-    csrf_token => Koha::Token->new->generate_csrf({ session_id => scalar $input->cookie('CGISESSID') }),
+    csrf_token => Koha::Token->new->generate_csrf( { session_id => scalar $input->cookie('CGISESSID') } ),
 );
 
 output_html_with_http_headers $input, $cookie, $template->output;
