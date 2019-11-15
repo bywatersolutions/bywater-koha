2d05047f0a37b3587e4922b8a1d43eae9b8b49b7
Kyle M Hall <kyle@bywatersolutions.com>
2019-11-15 14:23:14 -0500

    MDAH - Bug 8352 - Add print() method to Printer class

--- Koha/Schema/Result/Printer.pm
+++ Koha/Schema/Result/Printer.pm
@@ -69,6 +69,61 @@ __PACKAGE__->set_primary_key("printername");
 # Created by DBIx::Class::Schema::Loader v0.07025 @ 2013-10-14 20:56:21
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:OaIxwtdwn0TJLggwOygC2w
 
+sub print {
+    my ( $self, $params ) = @_;
+
+    my $data    = $params->{data};
+    my $is_html = $params->{is_html};
+
+    return unless ($data);
+
+    if ($is_html) {
+        require HTML::HTMLDoc;
+        my $htmldoc = new HTML::HTMLDoc();
+        $htmldoc->set_output_format('ps');
+        $htmldoc->set_html_content($data);
+        my $doc = $htmldoc->generate_pdf();
+        $data = $doc->to_string();
+    }
+
+    my ( $result, $error );
+
+    if ( $self->printqueue() =~ /:/ ) {
+
+        # Printer has a server:port address, use Net::Printer
+        require Net::Printer;
+
+        my ( $server, $port ) = split( /:/, $self->printqueue() );
+
+        my $printer = new Net::Printer(
+            printer     => $self->printernname(),
+            server      => $server,
+            port        => $port,
+            lineconvert => "YES"
+        );
+
+        $result = $printer->printstring($data);
+
+        $error = $printer->printerror();
+    }
+    else {
+        require Printer;
+
+        my $printer_name = $self->printername();
+
+        my $printer = new Printer( 'linux' => 'lp' );
+        $printer->print_command(
+            linux => {
+                type    => 'pipe',
+                command => "lp -d $printer_name",
+            }
+        );
+
+        $result = $printer->print($data);
+    }
+
+    return ( $result eq '1', $error );
+}
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
