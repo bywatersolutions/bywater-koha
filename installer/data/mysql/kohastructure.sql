ee9260d0d1a9bae76413c428fffc2445665cb057
Kyle M Hall <kyle@bywatersolutions.com>
2019-12-05 08:54:49 -0500

    ARLINGTON - Bug 24857: Update Database
    We have a partner that needs Volume Level Holds.
    This is a feature that some ILSs have that allows the creation of volumes at the bibliographic level,
    with items being optionally assigned to a given volume. This facilitates the ability to place holds
    at a volume level, rather than on a specific item or any available item on a record.
    
    Test Plan:
    1) Apply the patches for this bug
    2) Run updatedatabase.pl
    3) Restart all the things!
    4) Enable the new syspref EnableVolumes
    5) Browse to detail.pl for a record with items
    6) Note the new volumes tab
    7) Test creating, editing and deleting volumes
    8) On the Holdings tab, select one or more items using the checkboxes
    8) Note new selection options for setting a volume for items,
       and for clearing a volume for items.
    9) Test adding and clearing the volume set for items
    
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>
    
    Signed-off-by: Rebecca  Coert <rcoert@arlingtonva.us>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -1819,6 +1819,37 @@ CREATE TABLE `club_fields` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `volumes`
+--
+
+DROP TABLE IF EXISTS `volumes`;
+CREATE TABLE `volumes` ( -- information related to bibliographic records in Koha
+  `id` int(11) NOT NULL auto_increment, -- primary key, unique identifier assigned by Koha
+  `biblionumber` INT(11) NOT NULL default 0, -- foreign key linking this table to the biblio table
+  `display_order` INT(4) NOT NULL default 0, -- specifies the 'sort order' for volumes
+  `description` MEDIUMTEXT default NULL, -- equivilent to enumchron
+  `created_on` TIMESTAMP NULL, -- Time and date the volume was created
+  `updated_on` TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Time and date of the latest change on the volume (description)
+  PRIMARY KEY  (`id`),
+  CONSTRAINT `volumes_ibfk_1` FOREIGN KEY (`biblionumber`) REFERENCES `biblio` (`biblionumber`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Table structure for table `volume_items`
+--
+
+DROP TABLE IF EXISTS `volume_items`;
+CREATE TABLE `volume_items` ( -- information related to bibliographic records in Koha
+  `id` int(11) NOT NULL auto_increment, -- primary key, unique identifier assigned by Koha
+  `volume_id` int(11) NOT NULL default 0, -- foreign key making this table a 1 to 1 join from items to volumes
+  `itemnumber` int(11) NOT NULL default 0, -- foreign key linking this table to the items table
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY (volume_id,itemnumber),
+  CONSTRAINT `volume_items_iifk_1` FOREIGN KEY (`itemnumber`) REFERENCES `items` (`itemnumber`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `volume_items_vifk_1` FOREIGN KEY (`volume_id`) REFERENCES `volumes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
 -- Table structure for table `club_holds`
 --
 
