9411e065ddad0f35a875ebf23907fe065316597f
Nick Clemens <nick@bywatersolutions.com>
2020-01-23 02:35:38 +0000

    BWS-PKG - Bug 24476: 19.11.x rebase
    Signed-off-by: Andrew <andrew@bywatersolutions.com>
    
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Bug 24476: API Updates
    
    Signed-off-by: Andrew <andrew@bywatersolutions.com>
    
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Bug 24476: Change CanBookBeRenewed and adjust tests
    
    Signed-off-by: Andrew <andrew@bywatersolutions.com>
    
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Bug 24476: Allow patrons to opt out of auto-renewal
    
    To test:
    1 - Verify on staff side that patron can be edited to opt in our out of auto renewal
    2 - Check out some items to a patron opted in to auto renewal
    3 - Ensure the items are checked out and set to autorenew
    4 - Login on the opac at the patron
    5 - Verify items cannot be renewed as scheduled for auto-renewal
    6 - On staff side, opt patron out of auto renewal
    7 - Verify on opac items are no longer marked for auto renewal
    8 - Run the auto renewal cron job, items are not renewed
    9 - Set 'no renewal before' to a setting that would prevent renewal
    10 - Verify that opting patron in or out of auto renewal changes only the reason items cannot be renewed
    11 - Set 'no renewal before' to a setting that would allow for renewal
    12 - Verify that opting patron in/out changes their ability to renew
    13 - Verify that when opted out cron does not renew
    14 - Verify that when opted in the item is auto renewed
    15 - Reset the due date, opt out, verify manual renewal succeeds
    
    Signed-off-by: Andrew <andrew@bywatersolutions.com>
    
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Bug 24476: Rename autorenewal to autorenew_checkouts
    
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Bug 24476: Allow direct editing of autorenew_checkouts by patron
    
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Bug 24476: (QA follow-up) Standardize on 'auto renew(al)' and improve db update
    
    There were several versions: auto renew, auto-renew, autorenew
    I've decided to use auto renew (from automatic renewal) to keep
    things a bit more consistent.
    
    The DB update was missing AFTER and IGNORE for the sys pref.
    Also fixed a tiny typo (tp > to)
    Also changed
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Bug 24476: (QA follow-up) Move new option below warnings and messages in OPAC account
    
    This makes it so the new option appears right above the table of checkouts etc.
    but below any warnings, messages or dialogs.
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- Koha/Patron.pm
+++ Koha/Patron.pm
@@ -1609,6 +1609,7 @@ sub to_api_mapping {
         smsalertnumber      => 'sms_number',
         sort1               => 'statistics_1',
         sort2               => 'statistics_2',
+        autorenew_checkouts => 'autorenew_checkouts',
         streetnumber        => 'street_number',
         streettype          => 'street_type',
         zipcode             => 'postal_code',
