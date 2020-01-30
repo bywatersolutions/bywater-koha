4318cefa3a55a733c9d0b11a152ab2ee75bf6b80
Kyle M Hall <kyle@bywatersolutions.com>
2020-01-30 10:29:05 -0500

    BWS-PKG - Bug 24547: Add more action logs for holds
    It seems like ModReserveFill and ModReserveAffect should both produce action logs for holds.
    
    Test Plan:
    1) Apply this patch
    2) Place a hold
    3) Check in the item to trap the hold
    4) Check out the item to fill the hold
    5) Check the action logs for that reserve id
    6) Note the new logs!

--- C4/Reserves.pm
+++ C4/Reserves.pm
@@ -1008,6 +1008,9 @@ sub ModReserveFill {
         }
     );
 
+    logaction( 'HOLDS', 'MODIFY', $hold->reserve_id, Dumper($hold->unblessed) )
+        if C4::Context->preference('HoldsLog');
+
     # FIXME Must call Koha::Hold->cancel ? => No, should call ->filled and add the correct log
     Koha::Old::Hold->new( $hold->unblessed() )->store();
 
@@ -1104,6 +1107,9 @@ sub ModReserveAffect {
     if ( ( $item->location eq 'CART' && $item->permanent_location ne 'CART'  ) ) {
       CartToShelf( $itemnumber );
     }
+
+    logaction( 'HOLDS', 'MODIFY', $hold->reserve_id, Dumper($hold->unblessed) )
+        if C4::Context->preference('HoldsLog');
 
     return;
 }
