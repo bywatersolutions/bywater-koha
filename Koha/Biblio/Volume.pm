6f97e1e86adc4bdfbf6cef6b2f57401afebac66c
Tomas Cohen Arazi <tomascohen@theke.io>
2020-02-03 15:27:25 -0300

    ARLINGTON - Bug 24857: Koha::Biblio::Volume methods
    This patch introduces the following methods:
    - add_item
    - del_item
    - items
    - to_api_mapping
    - store (overloaded)

--- Koha/Biblio/Volume.pm
+++ Koha/Biblio/Volume.pm
@@ -19,6 +19,10 @@ use Modern::Perl;
 
 use base qw(Koha::Object);
 
+use Koha::Biblio::Volume::Items;
+use Koha::Exceptions::Object;
+use Koha::Items;
+
 =head1 NAME
 
 Koha::Volume - Koha Volume Object class
@@ -27,7 +31,97 @@ Koha::Volume - Koha Volume Object class
 
 =head2 Class methods
 
+=head3 store
+
+    $volume->store;
+
+Overloaded I<store> method that takes care of creation date handling.
+
 =cut
+
+sub store {
+    my ($self) = @_;
+
+    unless ( $self->in_storage ) {
+        # new entry
+        $self->set(
+            {
+                created_on => \'NOW()'
+            }
+        );
+    }
+
+    return $self->SUPER::store();
+}
+
+=head3 items
+
+    my $items = $volume->items;
+
+Returns all the items linked to the volume.
+
+=cut
+
+sub items {
+    my ($self) = @_;
+
+    my $items_rs = $self->_result->volume_items;
+    my @item_numbers = $items_rs->get_column('itemnumber')->all;
+
+    return unless @item_numbers;
+
+    return Koha::Items->search(
+        {
+            itemnumber => {
+                -in => \@item_numbers
+            }
+        }
+    );
+}
+
+=head3 add_item
+
+    $volume->add_item({ item_id => $item_id });
+
+=cut
+
+sub add_item {
+    my ($self, $params) = @_;
+
+    my $item_id = $params->{item_id};
+
+    my $item = Koha::Items->find( $item_id );
+    unless ( $item->biblionumber == $self->biblionumber ) {
+        Koha::Exceptions::Object::FKConstraint->throw(
+            broken_fk => 'biblionumber'
+        );
+    }
+
+    Koha::Biblio::Volume::Item->new(
+        {
+            itemnumber => $item_id,
+            volume_id  => $self->id
+        }
+    )->store;
+
+    return $self;
+}
+
+=head3 to_api_mapping
+
+This method returns the mapping for representing a Koha::Biblio::Volume object
+on the API.
+
+=cut
+
+sub to_api_mapping {
+    return {
+        id           => 'volume_id',
+        biblionumber => 'biblio_id',
+        created_on   => 'creation_date',
+        updated_on   => 'modification_date'
+    };
+}
 
 =head2 Internal methods
 
