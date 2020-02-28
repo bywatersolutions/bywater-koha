3b505aa6a41d4feac3fed6590c203de5c735d5c1
Kyle M Hall <kyle@bywatersolutions.com>
2020-02-28 10:51:47 -0500

    ARLINGTON - Bug 24860: Add new syspref EnableVolumeHolds

--- installer/data/mysql/sysprefs.sql
+++ installer/data/mysql/sysprefs.sql
@@ -679,6 +679,7 @@ INSERT INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `
 ('UseTransportCostMatrix','0','','Use Transport Cost Matrix when filling holds','YesNo'),
 ('UseWYSIWYGinSystemPreferences','0','','Show WYSIWYG editor when editing certain HTML system preferences.','YesNo'),
 ('EnableVolumes','0','','Enable volumes feature','YesNo'),
+('EnableVolumeHolds','0','','Enable volume level holds feature','YesNo'),
 ('viewISBD','1','','Allow display of ISBD view of bibiographic records','YesNo'),
 ('viewLabeledMARC','0','','Allow display of labeled MARC view of bibiographic records','YesNo'),
 ('viewMARC','1','','Allow display of MARC view of bibiographic records','YesNo'),
