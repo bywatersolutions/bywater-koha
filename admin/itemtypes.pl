3e2dce9cf0f0147a22f58a38d2bf79510d942f61
Kyle M Hall <kyle@bywatersolutions.com>
2020-03-09 13:11:55 -0400

    BWS-PKG - Bug 21443: Add ability to exclude holidays when calculating rentals fees by time period
    Bug 21443: Update database
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Marti Fuerst <mfuerst@hmcpl.org>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Bug 21443: Update Schema [DO NOT PUSH]
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Marti Fuerst <mfuerst@hmcpl.org>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Bug 21443: Add ability to exclude holidays when calculating rentals fees by time period
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Marti Fuerst <mfuerst@hmcpl.org>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Bug 21443: Unit tests
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Marti Fuerst <mfuerst@hmcpl.org>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Bug 21443: Mark new columns as boolean in schema file
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Bug 21443: Remove the dependency on finesCalendar
    
    Considering that the the use of finesCalendar for this calculation is
    already a binary choice, it makes sense to remove the use of
    finesCalendar here. It is an uneccessary complication that could
    introduce confusion. Allowing this new setting to directly control
    the behavior makes it clear and obvious what is going on.
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Bug 21443: (RM follow-up) Fix failing tests
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- admin/itemtypes.pl
+++ admin/itemtypes.pl
@@ -104,6 +104,8 @@ if ( $op eq 'add_form' ) {
     my $checkinmsgtype = $input->param('checkinmsgtype');
     my $hideinopac     = $input->param('hideinopac') // 0;
     my $searchcategory = $input->param('searchcategory');
+    my $rentalcharge_daily_calendar  = $input->param('rentalcharge_daily_calendar') // 0;
+    my $rentalcharge_hourly_calendar = $input->param('rentalcharge_hourly_calendar') // 0;
 
     if ( $itemtype and $is_a_modif ) {    # it's a modification
         $itemtype->description($description);
@@ -120,6 +122,8 @@ if ( $op eq 'add_form' ) {
         $itemtype->sip_media_type($sip_media_type);
         $itemtype->hideinopac($hideinopac);
         $itemtype->searchcategory($searchcategory);
+        $itemtype->rentalcharge_daily_calendar($rentalcharge_daily_calendar);
+        $itemtype->rentalcharge_hourly_calendar($rentalcharge_hourly_calendar);
 
         eval {
           $itemtype->store;
@@ -149,6 +153,8 @@ if ( $op eq 'add_form' ) {
                 sip_media_type      => $sip_media_type,
                 hideinopac          => $hideinopac,
                 searchcategory      => $searchcategory,
+                rentalcharge_daily_calendar  => $rentalcharge_daily_calendar,
+                rentalcharge_hourly_calendar => $rentalcharge_hourly_calendar,
             }
         );
         eval {
