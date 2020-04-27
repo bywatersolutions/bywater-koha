7ee05fe43a9e80b90ba61972bceea519afbf85b2
John Doe <you@example.com>
2020-04-27 14:42:39 +0000

    ARLINGTON - Fix unit test issues

---
+++ Koha/Schema/Result/VolumeItem.pm
@@ -0,0 +1,139 @@
+use utf8;
+package Koha::Schema::Result::VolumeItem;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Koha::Schema::Result::VolumeItem
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<volume_items>
+
+=cut
+
+__PACKAGE__->table("volume_items");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 volume_id
+
+  data_type: 'integer'
+  default_value: 0
+  is_foreign_key: 1
+  is_nullable: 0
+
+=head2 itemnumber
+
+  data_type: 'integer'
+  default_value: 0
+  is_foreign_key: 1
+  is_nullable: 0
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "volume_id",
+  {
+    data_type      => "integer",
+    default_value  => 0,
+    is_foreign_key => 1,
+    is_nullable    => 0,
+  },
+  "itemnumber",
+  {
+    data_type      => "integer",
+    default_value  => 0,
+    is_foreign_key => 1,
+    is_nullable    => 0,
+  },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+=head1 UNIQUE CONSTRAINTS
+
+=head2 C<volume_id>
+
+=over 4
+
+=item * L</volume_id>
+
+=item * L</itemnumber>
+
+=back
+
+=cut
+
+__PACKAGE__->add_unique_constraint("volume_id", ["volume_id", "itemnumber"]);
+
+=head1 RELATIONS
+
+=head2 itemnumber
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::Item>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "itemnumber",
+  "Koha::Schema::Result::Item",
+  { itemnumber => "itemnumber" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+=head2 volume
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::Volume>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "volume",
+  "Koha::Schema::Result::Volume",
+  { id => "volume_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07046 @ 2020-04-27 14:43:18
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:mxvVv6hxrvEpT1uHzPB70Q
+
+sub koha_object_class {
+        'Koha::Biblio::Volume::Item';
+}
+sub koha_objects_class {
+        'Koha::Biblio::Volume::Items';
+}
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
