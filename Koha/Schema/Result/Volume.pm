29b549d87a2231a94b71291539c2f53c67d76ca4
John Doe <you@example.com>
2020-04-27 14:42:39 +0000

    ARLINGTON - Fix unit test issues

---
+++ Koha/Schema/Result/Volume.pm
@@ -0,0 +1,180 @@
+use utf8;
+package Koha::Schema::Result::Volume;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Koha::Schema::Result::Volume
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<volumes>
+
+=cut
+
+__PACKAGE__->table("volumes");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 biblionumber
+
+  data_type: 'integer'
+  default_value: 0
+  is_foreign_key: 1
+  is_nullable: 0
+
+=head2 display_order
+
+  data_type: 'integer'
+  default_value: 0
+  is_nullable: 0
+
+=head2 description
+
+  data_type: 'mediumtext'
+  is_nullable: 1
+
+=head2 created_on
+
+  data_type: 'timestamp'
+  datetime_undef_if_invalid: 1
+  is_nullable: 1
+
+=head2 updated_on
+
+  data_type: 'timestamp'
+  datetime_undef_if_invalid: 1
+  default_value: current_timestamp
+  is_nullable: 0
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "biblionumber",
+  {
+    data_type      => "integer",
+    default_value  => 0,
+    is_foreign_key => 1,
+    is_nullable    => 0,
+  },
+  "display_order",
+  { data_type => "integer", default_value => 0, is_nullable => 0 },
+  "description",
+  { data_type => "mediumtext", is_nullable => 1 },
+  "created_on",
+  {
+    data_type => "timestamp",
+    datetime_undef_if_invalid => 1,
+    is_nullable => 1,
+  },
+  "updated_on",
+  {
+    data_type => "timestamp",
+    datetime_undef_if_invalid => 1,
+    default_value => \"current_timestamp",
+    is_nullable => 0,
+  },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+=head1 RELATIONS
+
+=head2 biblionumber
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::Biblio>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "biblionumber",
+  "Koha::Schema::Result::Biblio",
+  { biblionumber => "biblionumber" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+=head2 old_reserves
+
+Type: has_many
+
+Related object: L<Koha::Schema::Result::OldReserve>
+
+=cut
+
+__PACKAGE__->has_many(
+  "old_reserves",
+  "Koha::Schema::Result::OldReserve",
+  { "foreign.volume_id" => "self.id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+=head2 reserves
+
+Type: has_many
+
+Related object: L<Koha::Schema::Result::Reserve>
+
+=cut
+
+__PACKAGE__->has_many(
+  "reserves",
+  "Koha::Schema::Result::Reserve",
+  { "foreign.volume_id" => "self.id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+=head2 volume_items
+
+Type: has_many
+
+Related object: L<Koha::Schema::Result::VolumeItem>
+
+=cut
+
+__PACKAGE__->has_many(
+  "volume_items",
+  "Koha::Schema::Result::VolumeItem",
+  { "foreign.volume_id" => "self.id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07046 @ 2020-04-27 14:43:18
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:gtLUXYDkFtTN7LU5ulousA
+
+sub koha_object_class {
+        'Koha::Biblio::Volume';
+}
+sub koha_objects_class {
+        'Koha::Biblio::Volumes';
+}
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
