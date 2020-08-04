03dff1ba2a77a32edcf84bf8425d4822246171d5
Kyle M Hall <kyle@bywatersolutions.com>
2020-08-04 12:53:47 -0400

    BWS-PKG - Add bwsbranch to the About page

--- about.pl
+++ about.pl
@@ -951,4 +951,11 @@ if ( $tab eq 'database' ) {
     my $db_audit   = $db_auditor->run;
     $template->param( db_audit => $db_audit );
 }
+
+my $bwsbranch_filename = '/usr/share/koha/bin/bwsbranch';
+if ( -r $bwsbranch_filename ) {
+    my $bwsbranch = read_file( $bwsbranch_filename );
+    $template->param( bwsbranch => $bwsbranch );
+}
+
 output_html_with_http_headers $query, $cookie, $template->output;
