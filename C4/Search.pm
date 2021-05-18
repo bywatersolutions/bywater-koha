3c3346822f20bcfd85354dafc8084dfa18891cea
Nick Clemens <nick@bywatersolutions.com>
2021-05-18 15:06:43 +0000

    MONTGOMERY - Bug 28373: [21.05.x] Add PassItemMarcToXSLT system preference
    Default stylesheets do not reference item fields for XSLT display, however, we
    spend time translating the values in the item fields.
    
    This patch adds a system preference, PassItemMarcToXSLT. and unless enabled we remove
    item fields before processing
    
    To test:
    1 - Perform some search on the staff client and opac
    2 - Use the console (F12) to view the time spent on the network tab
    3 - Note performance
    4 - Apply patch, updatedatabase, restart_all
    5 - Repeat searches
    6 - Note that display has not changed
    7 - Note performance, results should display slightly faster
    
    Signed-off-by: Emmi Takkinen <emmi.takkinen@koha-suomi.fi>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Bug 28373: Unit test
    
    Signed-off-by: Emmi Takkinen <emmi.takkinen@koha-suomi.fi>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Bug 28373: (QA follow-up) Fix typo in system preference description
    
    Signed-off-by: Emmi Takkinen <emmi.takkinen@koha-suomi.fi>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Bug 28373: (follow-up) QA fixes
    
    Use fully qualified subroutine name
    Don't return explicit undef
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Bug 28373: (QA follow-up) Add . to end of system preference description
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Bug 28373: Add note to XSLT prefs about the new pref
    
    Bug 28373: Add warning if custom stylesheets are defined
    
    This patch adds a check of the current XSLT prefs and warns to check the new pref
    if any are not set to default

--- C4/Search.pm
+++ C4/Search.pm
@@ -1791,6 +1791,7 @@ sub searchResults {
 
         # Pull out the items fields
         my @fields = $marcrecord->field($itemtag);
+        $marcrecord->delete_fields( @fields ) unless C4::Context->preference('PassItemMarcToXSLT');
         my $marcflavor = C4::Context->preference("marcflavour");
 
         # adding linked items that belong to host records
