dd1e33c8f0df0bb2d09e0df95d0edab6f6597338
Kyle Hall <kyle@bywatersolutions.com>
2021-09-23 13:24:42 -0400

    BWS-PKG - Bug 29100: Add checkouts data loop to predue notices script ( advance_notices.pl )
    Predue / due notices are limited to using itemscontent to display checkouts data. It would be nice to make all the checkouts data available for those notices so that libraries could format that data more nicely if they wish.
    
    1) Apply this patch
    2) For DUE and PREDUE notices, set the message body to the following:
    Title: [% issue.title %]
    3) For DUEDGST and PREDUEDGST notices, set the message body to the following:
    Titles:
    [% FOREACH i IN issues %]
    * [% i.title %]
    [% END %]
    4) Generate PREDUE and DUE notices for patrons including digests
    5) Verify those notices contain the checkout titles

--- misc/cronjobs/advance_notices.pl
+++ misc/cronjobs/advance_notices.pl
@@ -291,10 +291,8 @@ UPCOMINGITEM: foreach my $upcoming ( @$upcoming_dues ) {
             my $item = Koha::Items->find( $upcoming->{itemnumber} );
             my $letter_type = 'DUE';
             $sth->execute($upcoming->{'borrowernumber'},$upcoming->{'itemnumber'},'0');
-            my $titles = "";
-            while ( my $item_info = $sth->fetchrow_hashref()) {
-                $titles .= C4::Letters::get_item_content( { item => $item_info, item_content_fields => \@item_content_fields } );
-            }
+            my $item_info = $sth->fetchrow_hashref();
+            my $title = C4::Letters::get_item_content( { item => $item_info, item_content_fields => \@item_content_fields } );
 
             ## Get branch info for borrowers home library.
             foreach my $transport ( keys %{$borrower_preferences->{'transports'}} ) {
@@ -303,7 +301,10 @@ UPCOMINGITEM: foreach my $upcoming ( @$upcoming_dues ) {
                                       branchcode     => $branchcode,
                                       biblionumber   => $item->biblionumber,
                                       itemnumber     => $upcoming->{'itemnumber'},
-                                      substitute     => { 'items.content' => $titles },
+                                      substitute     => {
+                                          'items.content' => $title,
+                                          issue           => $item_info,
+                                      },
                                       message_transport_type => $transport,
                                     } )
                     or warn "no letter of type '$letter_type' found for borrowernumber ".$upcoming->{'borrowernumber'}.". Please see sample_notices.sql";
@@ -338,10 +339,8 @@ UPCOMINGITEM: foreach my $upcoming ( @$upcoming_dues ) {
             my $item = Koha::Items->find( $upcoming->{itemnumber} );
             my $letter_type = 'PREDUE';
             $sth->execute($upcoming->{'borrowernumber'},$upcoming->{'itemnumber'},$borrower_preferences->{'days_in_advance'});
-            my $titles = "";
-            while ( my $item_info = $sth->fetchrow_hashref()) {
-                $titles .= C4::Letters::get_item_content( { item => $item_info, item_content_fields => \@item_content_fields } );
-            }
+            my $item_info = $sth->fetchrow_hashref();
+            my $title = C4::Letters::get_item_content( { item => $item_info, item_content_fields => \@item_content_fields } );
 
             ## Get branch info for borrowers home library.
             foreach my $transport ( keys %{$borrower_preferences->{'transports'}} ) {
@@ -350,7 +349,10 @@ UPCOMINGITEM: foreach my $upcoming ( @$upcoming_dues ) {
                                       branchcode     => $branchcode,
                                       biblionumber   => $item->biblionumber,
                                       itemnumber     => $upcoming->{'itemnumber'},
-                                      substitute     => { 'items.content' => $titles },
+                                      substitute     => {
+                                          'items.content' => $title,
+                                          issue           => $item_info,
+                                      },
                                       message_transport_type => $transport,
                                     } )
                     or warn "no letter of type '$letter_type' found for borrowernumber ".$upcoming->{'borrowernumber'}.". Please see sample_notices.sql";
@@ -589,7 +591,9 @@ sub send_digests {
         });
         my $titles = "";
         my @itemnumbers;
+        my @issues;
         while ( my $item_info = $next_item_info->()) {
+            push( @issues, $item_info );
             push @itemnumbers, $item_info->{itemnumber};
             $titles .= C4::Letters::get_item_content( { item => $item_info, item_content_fields => \@item_content_fields } );
         }
@@ -599,9 +603,11 @@ sub send_digests {
                 {
                     letter_code    => $params->{letter_code},
                     borrowernumber => $borrowernumber,
+                    issues         => \@issues,
                     substitute     => {
                         count           => $count,
                         'items.content' => $titles,
+                        issues          => \@issues,
                         %branch_info
                     },
                     itemnumbers    => \@itemnumbers,
