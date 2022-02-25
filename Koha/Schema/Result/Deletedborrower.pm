8e0f2278ce05f4175da2b8584e247a035cc978f6
Nick Clemens <nick@bywatersolutions.com>
2022-02-25 12:01:42 +0000

    CDOC - Bug 29924: DO NOT PUSH SCHEMA UPDATES

--- Koha/Schema/Result/Deletedborrower.pm
+++ Koha/Schema/Result/Deletedborrower.pm
@@ -294,6 +294,14 @@ date the patron was added to Koha (YYYY-MM-DD)
 
 date the patron/borrower's card is set to expire (YYYY-MM-DD)
 
+=head2 password_expiration_date
+
+  data_type: 'date'
+  datetime_undef_if_invalid: 1
+  is_nullable: 1
+
+date the patron/borrower's password is set to expire (YYYY-MM-DD)
+
 =head2 date_renewed
 
   data_type: 'date'
@@ -673,6 +681,8 @@ __PACKAGE__->add_columns(
   { data_type => "date", datetime_undef_if_invalid => 1, is_nullable => 1 },
   "dateexpiry",
   { data_type => "date", datetime_undef_if_invalid => 1, is_nullable => 1 },
+  "password_expiration_date",
+  { data_type => "date", datetime_undef_if_invalid => 1, is_nullable => 1 },
   "date_renewed",
   { data_type => "date", datetime_undef_if_invalid => 1, is_nullable => 1 },
   "gonenoaddress",
@@ -775,8 +785,8 @@ __PACKAGE__->add_columns(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07049 @ 2021-06-07 05:28:04
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:/9VjUuilKGDwV/pLlf/4ew
+# Created by DBIx::Class::Schema::Loader v0.07049 @ 2022-02-25 11:49:33
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:bHVaOFyEIfwcCxhHB2ItsA
 
 __PACKAGE__->add_columns(
     '+anonymized'    => { is_boolean => 1 },
