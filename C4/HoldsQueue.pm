ab8e50237b2c603e06ca6f0c01822fc635bcae29
Nick Clemens <nick@bywatersolutions.com>
2022-03-23 15:16:34 +0000

    MONTGOMERY - Bug 29507: Speed up auto renew cronjob via parallel processing
    The cron can take a very long time to run on systems with many issues. For example, a partner with ~250k auto_renew issues is taking about 9 hours to run.
    
    If we run that same number of holds in 5 parallel chunks ( splitting the number of issues as evenly as possible, it could take under 2 hours.
    
    Test Plan:
    0) Install the Perl library Parallel::ForkManager
    1) Generate a number of issues marked for auto_renew
    2) Run the automatic_renewals.pl, use the `time` utility to track how much time it took to run
    3) Set parallel_loops to 10 in auto_renew_cronjob section of config in koha-conf
    4) Repeat step 2, note the improvement in speed
    5) Experiment with other values

--- C4/HoldsQueue.pm
+++ C4/HoldsQueue.pm
@@ -271,6 +271,14 @@ sub CreateQueue {
     } else {
         _ProcessBiblios( $bibs_with_pending_requests, $branches_to_use, $transport_cost_matrix, $dbh );
     }
+
+    my $do_not_lock = ( exists $ENV{_} && $ENV{_} =~ m|prove| ) || $ENV{KOHA_TESTING};
+    $dbh->{AutoCommit} = 0 unless $do_not_lock;
+    $dbh->do("DELETE FROM tmp_holdsqueue");  # clear the old tables for new info
+    $dbh->do("DELETE FROM hold_fill_targets");
+    $dbh->do("INSERT INTO tmp_holdsqueue SELECT * FROM tmp_holdsqueue_builder");
+    $dbh->do("INSERT INTO hold_fill_targets SELECT * FROM hold_fill_targets_builder");
+    $dbh->commit() unless $do_not_lock;
 }
 
 =head2 _ProcessBiblios
@@ -302,14 +310,6 @@ sub _ProcessBiblios {
             #warn Dumper($hold_requests), Dumper($available_items), Dumper($item_map);
         }
     }
-
-    my $do_not_lock = ( exists $ENV{_} && $ENV{_} =~ m|prove| ) || $ENV{KOHA_TESTING};
-    $dbh->{AutoCommit} = 0 unless $do_not_lock;
-    $dbh->do("DELETE FROM tmp_holdsqueue");  # clear the old tables for new info
-    $dbh->do("DELETE FROM hold_fill_targets");
-    $dbh->do("INSERT INTO tmp_holdsqueue SELECT * FROM tmp_holdsqueue_builder");
-    $dbh->do("INSERT INTO hold_fill_targets SELECT * FROM hold_fill_targets_builder");
-    $dbh->commit() unless $do_not_lock;
 }
 
 =head2 GetBibsWithPendingHoldRequests
