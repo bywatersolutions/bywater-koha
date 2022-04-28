f89cd940610a2855579b674906e5cb72973cc27e
Nick Clemens <nick@bywatersolutions.com>
2022-04-28 17:54:48 +0000

    ARLINGTON - Bug 24857: Add object classes to Schema

--- Koha/Schema/Result/ItemGroupItem.pm
+++ Koha/Schema/Result/ItemGroupItem.pm
@@ -132,6 +132,11 @@ __PACKAGE__->belongs_to(
 # Created by DBIx::Class::Schema::Loader v0.07049 @ 2022-05-27 11:50:53
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:GYLsB8KfcS/OSHtkWIxA6Q
 
+sub koha_object_class {
+    'Koha::Biblio::ItemGroup::Item';
+}
+sub koha_objects_class {
+    'Koha::Biblio::ItemGroup::Items';
+}
 
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
