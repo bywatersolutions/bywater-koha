53e51fe6f7c93835485359b486ccbb52398463e5
Wm. Nick Clemens <nick@bywatersolutions.com>
2022-07-11 12:44:28 -0400

    Update pages.pl for bug 17600
    Implicitly import get_template_and_user

--- opac/pages.pl
+++ opac/pages.pl
@@ -20,7 +20,7 @@
 
 use Modern::Perl;
 use CGI qw ( -utf8 );
-use C4::Auth;    # get_template_and_user
+use C4::Auth qw(get_template_and_user);
 use C4::Output;
 use C4::Languages qw(getTranslatedLanguages accept_language);
 use Koha::Quotes;
