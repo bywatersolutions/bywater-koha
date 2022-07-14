f97e134dbb6a670d464741b15b49b1f8707e20af
Kyle M Hall <kyle@bywatersolutions.com>
2022-07-14 14:54:41 +0000

    CDOC - Update DB Schema files

--- Koha/Schema/Result/Category.pm
+++ Koha/Schema/Result/Category.pm
@@ -54,6 +54,13 @@ number of months the patron is enrolled for (will be NULL if enrolmentperioddate
 
 date the patron is enrolled until (will be NULL if enrolmentperiod is set)
 
+=head2 password_expiry_days
+
+  data_type: 'smallint'
+  is_nullable: 1
+
+number of days after which the patron must reset their password
+
 =head2 upperagelimit
 
   data_type: 'smallint'
@@ -201,6 +208,8 @@ __PACKAGE__->add_columns(
   { data_type => "smallint", is_nullable => 1 },
   "enrolmentperioddate",
   { data_type => "date", datetime_undef_if_invalid => 1, is_nullable => 1 },
+  "password_expiry_days",
+  { data_type => "smallint", is_nullable => 1 },
   "upperagelimit",
   { data_type => "smallint", is_nullable => 1 },
   "dateofbirthrequired",
@@ -329,8 +338,8 @@ __PACKAGE__->has_many(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07049 @ 2021-11-19 14:20:11
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:xrnHXXv5sd3S2sMEKzHLmA
+# Created by DBIx::Class::Schema::Loader v0.07049 @ 2022-07-14 14:53:52
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:Lr3PwwNlleIGWpQ10KWi7w
 
 __PACKAGE__->add_columns(
     '+exclude_from_local_holds_priority' => { is_boolean => 1 },
