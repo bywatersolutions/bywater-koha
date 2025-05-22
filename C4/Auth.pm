be098cde7d18459d15439cfef2e5aac038737876
Kyle M Hall <kyle@bywatersolutions.com>
2025-05-22 07:41:44 -0400

    BWS-PKG - Log start and end of processes and requests

--- C4/Auth.pm
+++ C4/Auth.pm
@@ -841,6 +841,7 @@ sub _timeout_syspref {
 
 sub checkauth {
     my $query = shift;
+    warn "REQUEST STARTING [$$]";
 
     # Get shibboleth login attribute
     my $shib       = C4::Context->config('useshibboleth') && shib_ok();
