b671634a6fcc23ffef0abe055bb9e9bab29de7d7
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2026-01-21 15:36:16 +0000

    MAINLIB - Bug 39658: API: Add REST endpoints for patron account links
    Endpoints: GET/POST/DELETE /patrons/{id}/account_links
    
    Signed-off-by: Trevor Diamond <trevor.diamond@mainlib.org>
    Signed-off-by: Kellie Stephens <kellie.stephens@bywatersolutions.com>
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

---
+++ Koha/REST/V1/Patrons/AccountLinks.pm
@@ -0,0 +1,141 @@
+package Koha::REST::V1::Patrons::AccountLinks;
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <https://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Mojo::Base 'Mojolicious::Controller';
+
+use Koha::Patrons;
+
+use Scalar::Util qw( blessed );
+use Try::Tiny    qw( catch try );
+
+=head1 NAME
+
+Koha::REST::V1::Patrons::AccountLinks
+
+=head1 API
+
+=head2 Methods
+
+=head3 list
+
+Controller function that handles listing Koha::Patron::AccountLink objects for the requested patron
+
+=cut
+
+sub list {
+    my $c = shift->openapi->valid_input or return;
+
+    my $patron = Koha::Patrons->find( $c->param('patron_id') );
+
+    return $c->render_resource_not_found("Patron")
+        unless $patron;
+
+    return try {
+        my $links_set = $patron->linked_account_links;
+
+        return $c->render( status => 200, openapi => [] )
+            unless $links_set;
+
+        my $links = $c->objects->search($links_set);
+        return $c->render( status => 200, openapi => $links );
+    } catch {
+        $c->unhandled_exception($_);
+    };
+}
+
+=head3 add
+
+Controller function that handles linking a patron to another account
+
+=cut
+
+sub add {
+    my $c = shift->openapi->valid_input or return;
+
+    my $patron = Koha::Patrons->find( $c->param('patron_id') );
+
+    return $c->render_resource_not_found("Patron")
+        unless $patron;
+
+    my $body             = $c->req->json;
+    my $linked_patron_id = $body->{linked_patron_id};
+    my $linked_patron    = Koha::Patrons->find($linked_patron_id);
+
+    return $c->render_resource_not_found("Linked patron")
+        unless $linked_patron;
+
+    return try {
+        my $link = $patron->link_to_patron($linked_patron);
+
+        $c->res->headers->location( $c->req->url->to_string . '/' . $link->id );
+        return $c->render(
+            status  => 201,
+            openapi => $c->objects->to_api($link),
+        );
+    } catch {
+        if ( blessed $_ ) {
+            if ( $_->isa('Koha::Exceptions::PatronAccountLink::AlreadyLinked') ) {
+                return $c->render(
+                    status  => 409,
+                    openapi => { error => $_->description, error_code => 'AlreadyLinked' }
+                );
+            } elsif ( $_->isa('Koha::Exceptions::PatronAccountLink::DifferentGroups') ) {
+                return $c->render(
+                    status  => 409,
+                    openapi => { error => $_->description, error_code => 'DifferentGroups' }
+                );
+            }
+        }
+        $c->unhandled_exception($_);
+    };
+}
+
+=head3 delete
+
+Controller method that handles removing a patron account link
+
+=cut
+
+sub delete {
+    my $c = shift->openapi->valid_input or return;
+
+    my $patron = Koha::Patrons->find( $c->param('patron_id') );
+
+    return $c->render_resource_not_found("Patron")
+        unless $patron;
+
+    return try {
+        my $links_set = $patron->linked_account_links;
+
+        return $c->render_resource_not_found("Account link")
+            unless $links_set;
+
+        my $link = $links_set->find( $c->param('account_link_id') );
+
+        return $c->render_resource_not_found("Account link")
+            unless $link;
+
+        $link->delete;
+        return $c->render_resource_deleted;
+    } catch {
+        $c->unhandled_exception($_);
+    };
+}
+
+1;
