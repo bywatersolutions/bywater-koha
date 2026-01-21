7c69fa19dcce7e49793da98c29c25a69675de0f3
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2026-01-21 15:36:17 +0000

    MAINLIB - Bug 39658: DBIC schema - DO NOT PUSH
    Auto-generated schema for patron_account_links table.
    
    Signed-off-by: Trevor Diamond <trevor.diamond@mainlib.org>
    Signed-off-by: Kellie Stephens <kellie.stephens@bywatersolutions.com>
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- Koha/Schema/Result/Borrower.pm
+++ Koha/Schema/Result/Borrower.pm
@@ -1676,6 +1676,21 @@ __PACKAGE__->has_many(
   { cascade_copy => 0, cascade_delete => 0 },
 );
 
+=head2 patron_account_link
+
+Type: might_have
+
+Related object: L<Koha::Schema::Result::PatronAccountLink>
+
+=cut
+
+__PACKAGE__->might_have(
+  "patron_account_link",
+  "Koha::Schema::Result::PatronAccountLink",
+  { "foreign.borrowernumber" => "self.borrowernumber" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
 =head2 patron_consents
 
 Type: has_many
@@ -2212,8 +2227,8 @@ Composing rels: L</user_permissions> -> permission
 __PACKAGE__->many_to_many("permissions", "user_permissions", "permission");
 
 
-# Created by DBIx::Class::Schema::Loader v0.07051 @ 2025-10-29 16:44:27
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:SSJcjPvlr82qecmbk1Q8iA
+# Created by DBIx::Class::Schema::Loader v0.07051 @ 2026-01-19 13:41:13
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:ri+7zHVsNeXiewOHao3A1Q
 
 __PACKAGE__->belongs_to(
   "library",
