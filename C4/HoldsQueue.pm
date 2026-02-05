f476016d898c65be8c6b3afc18bd8535a46cc37e
Kyle M Hall <kyle@bywatersolutions.com>
2026-02-05 16:10:59 -0500

    Bug 41781 - Holds queue builder ( build_holds_queue.pl ) fails if HoldsQueueParallelLoopsCount is greater than 1
    If HoldsQueueParallelLoopsCount is set to a value greater than 1, build_holds_queue.pl will fail with the message:
    
    Can't locate object method "new" via package "Parallel::ForkManager" (perhaps you forgot to load "Parallel::ForkManager"?) at /kohadevbox/koha/C4/HoldsQueue.pm line 205.
    
    Test Plan:
    1) Run misc/cronjobs/holds/build_holds_queue.pl
    2) Note the error message
    3) Run misc/cronjobs/holds/build_holds_queue.pl
    4) Note the error message is gone!

--- C4/HoldsQueue.pm
+++ C4/HoldsQueue.pm
@@ -34,6 +34,7 @@ use Koha::Patrons;
 use List::Util         qw( shuffle );
 use List::MoreUtils    qw( any );
 use Algorithm::Munkres qw();
+use Parallel::ForkManager;
 
 our ( @ISA, @EXPORT_OK );
 
